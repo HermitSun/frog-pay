5506687da0b8d39fbdef41c5ca44d4597e22dda2
Wen Sun <syl1887415157@126.com>
2019-05-16 20:27:35 +0800

    feat: 迭代一

--- src/router/routes.js
+++ src/router/routes.js
@@ -1,13 +1,5 @@
 export default [
   {
-    path: '/',
-    name: '首页',
-    component: () => import('@/views/Index.vue'),
-    meta: {
-      title: '欢迎来到xxx公司'
-    }
-  },
-  {
     path: '/payment/:id',
     name: '支付',
     component: () => import('@/views/payment/Payment.vue'),
